b2c1579b4ca241a62a0a590e884a63a30c022b28
ShuttleLickingToesIfYouLookMyScript <79196135+Error-virus@users.noreply.github.com>
2022-05-27 19:10:58 +0800

    Update vape v4 fixed version.lua

--- vape v4 fixed version.lua
+++ vape v4 fixed version.lua
@@ -3,3 +3,8 @@ Title = "Announcement";
 Text = "maintenance because lyt warned me";
 Duration = math.huge;
 }) 
+game.StarterGui:SetCore("SendNotification", {
+Title = "Announcement";
+Text = "also trying to fix assets bug";
+Duration = math.huge;
+}) 
