f5c0f06b58406b749937d8e5c0d8a9bf562af6d3
ShuttleLickingToesIfYouLookMyScript <79196135+Error-virus@users.noreply.github.com>
2022-06-07 23:12:49 +0800

    Update vape v4 fixed version.lua

--- vape v4 fixed version.lua
+++ vape v4 fixed version.lua
@@ -1,1 +1,8 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "Announcement";
+Text = "fat nuts";
+Duration = 10
+})
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
