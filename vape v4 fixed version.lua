8860a259e407f5e1fefcf4882087e169087f188e
ShuttleLickingToesIfYouLookMyScript <79196135+Error-virus@users.noreply.github.com>
2022-06-26 23:38:55 +0800

    Update vape v4 fixed version.lua

--- vape v4 fixed version.lua
+++ vape v4 fixed version.lua
@@ -1,11 +1,21 @@
+local function callback(text)
+    if text == "Ok" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+    elseif text == "Decline" then
+        return
+    end
+end
+local bindableFunction = Instance.new("BindableFunction")
+bindableFunction.OnInvoke = callback
+
 game.StarterGui:SetCore("SendNotification", {
-Title = "Announcement";
-Text = "fat nuts";
-Duration = 10
-})
+Title = "Alert!";
+Text = "Use alt right now due of bedwars using contentprovider to detect gui";
+Callback = bindableFunction;
+Button1 = "Ok";
+Button2 = "Decline";
+Duration = math.huge;
+}) 
 
 
-game.Players.LocalPlayer:Kick("ALERT: bedwars detecting all gui")
 
-wait(5)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
