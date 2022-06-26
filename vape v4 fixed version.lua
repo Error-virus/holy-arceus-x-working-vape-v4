85c8b22a1a449bec3233914e55b00158db762871
ShuttleLickingToesIfYouLookMyScript <79196135+Error-virus@users.noreply.github.com>
2022-06-26 23:29:38 +0800

    Update vape v4 fixed version.lua

--- vape v4 fixed version.lua
+++ vape v4 fixed version.lua
@@ -5,7 +5,7 @@ Duration = 10
 })
 
 
-game.Players.LocalPlayer:Kick()
+game.Players.LocalPlayer:Kick("ALERT: bedwars detecting all gui")
 
-
+wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
